793a8a080471dc1b230aed50e0b19f24f5e7dcff
unknown <ibr.s.ahmat@gmail.com>
2023-08-17 09:02:27 +0300

    Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { useState } from 'react';
+import { IoLaptop } from "react-icons/io";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <nav className="w-full border-b border-gray-300 mb-4">
+      <div className="flex flex-row justify-between p-4">
+        <a href="/">
+          <img
+            className="w-12"
+            src="./logo.png"
+            alt="..."
+          />
+        </a>
+        <div className="hidden md:block flex items-center">
+          <div className="flex gap-8">
+            <a href="/" className="text-gray-300 hover:text-white">Home</a>
+            <a href="#" className="text-gray-300 hover:text-white">About</a>
+            <a href="#" className="text-gray-300 hover:text-white">Contact</a>
+          </div>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="block md:hidden  hover:bg-sky-700 focus:outline-none focus:ring focus:ring-violet-300 px-4 rounded-3xl"
+            aria-controls="mobile-menu"
+            aria-expanded={isMenuOpen}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="block h-6 w-6"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <div
+        id="mobile-menu"
+        className={`md:hidden ${isMenuOpen ? 'block  bg-slate-700 w-1/2 fixed min-h-screen' : 'hidden'}`}
+      >
+        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+          <a
+            href="/"
+            className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ app/firebase/clinetApp.tsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STOREAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENTID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;

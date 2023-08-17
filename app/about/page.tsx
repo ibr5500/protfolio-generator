54b063e35ce6c29d16342ec57d4631a8f98f7eec
unknown <ibr.s.ahmat@gmail.com>
2023-08-17 13:42:57 +0300

    Fix escaped

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,16 +13,16 @@ const About = () => {
         />
         <div className="text-gray-300">
           <h2 className="text-2xl text-lime-200 mb-1 animate-pulse">
-            Hello I'm Jhon doe a software developer
+            Hello I&apos;m Jhon doe a software developer
           </h2>
           <p>
             It is a long established fact that a reader will be distracted by
             the readable content of a page when looking at its layout. The point
             of using Lorem Ipsum is that it has a more-or-less normal distribution
-            of letters, as opposed to using 'Content here, content here', making
+            of letters, as opposed to using &apos;Content here, content here&apos;, making
             it look like readable English. Many desktop publishing packages and
             web page editors now use Lorem Ipsum as their default model text, and
-            a search for 'lorem ipsum' will uncover many web sites still in their
+            a search for &apos;lorem ipsum&apos; will uncover many web sites still in their
             infancy. Various versions have evolved over the years, sometimes by
             accident, sometimes on purpose (injected humour and the like).
           </p>
